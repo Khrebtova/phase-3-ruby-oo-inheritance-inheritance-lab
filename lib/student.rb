8fb4e1e9bcc8aa860714a21bc19569926a0db787
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-03-23 11:26:23 -0700

    commit tree 95ab0bbe8eabf445e04fd82e09e6af4a1d827c38

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+require 'pry'
+require_relative './user'
+
+class Student < User
+    def initialize(knowledge = [])
+        @knowledge=knowledge
+    end 
+    
+    def learn(new_knowledge)
+        @knowledge << new_knowledge
+    end
+    
+    def knowledge
+        @knowledge
+    end
 
 end
+
+
